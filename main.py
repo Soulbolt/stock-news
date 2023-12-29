4bca8136ac48461fe31a5c179710503a48fb67c1
Soulbolt <soulbolt@gmail.com>
2023-12-29 12:17:52 -0700

    implemeted module to securely access secret keys without exposing it on base code. Added code for end point, parameters to access stocks data for the month of November and access a particulate date's closing amount.

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
+import os
+from dotenv import load_dotenv
+import requests
+
+load_dotenv()
+
+API_KEY = os.getenv('STOCKS_API_KEY')
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
-
+END_POINT = "https://www.alphavantage.co/query?"
+parameters = {
+    "function": "TIME_SERIES_INTRADAY",
+    "symbol": "TSLA",
+    "interval": "60min",
+    "month": "2023-11",
+    "outputsize": "compact",
+    "apikey": API_KEY,
+}
 ## STEP 1: Use https://www.alphavantage.co
 # When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
+response = requests.get(url=END_POINT, params= parameters)
+response.raise_for_status()
+data_news = response.json()
+print(data_news["Time Series (60min)"]["2023-11-30 19:00:00"]["4. close"])
 
 ## STEP 2: Use https://newsapi.org
 # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
